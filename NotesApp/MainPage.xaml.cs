4d0795a48f4775141879723ccfde92353c14a061
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2025-02-28 22:16:49 -0300

    Projeto NotesApp

---
+++ NotesApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace NotesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        string caminho = FileSystem.AppDataDirectory;
+        string arquivo = "arquivo.txt";
+
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+            if (File.Exists(Path.Combine(caminho, arquivo))){
+                ConteudoEditor.Text = File.ReadAllText(Path.Combine(caminho, arquivo));
+            }
+        }
+
+        private void SalvarButton_Clicked(object sender, EventArgs e)
+        {
+
+            string conteudo = ConteudoEditor.Text;
+            File.WriteAllText(Path.Combine(caminho, arquivo),conteudo);
+        }
+
+        private void DeletarButton_Clicked(object sender, EventArgs e)
+        {
+            File.Delete(Path.Combine(caminho, arquivo));
+            DisplayAlert("Arquivo apagado", $"O arquivo {arquivo}  foi apagado com sucesso.", "Ok");
+            ConteudoEditor.Text = "";
+        }
+    }
+
+}
